70aad272ba971ac485f23bebcd5ca148e8b6e2e1
stranger7771981 <denis_uglov@mail>
2018-12-07 16:57:36 +0600

    Change Street_osh23

--- src/example/street_osh.py
+++ src/example/street_osh.py
@@ -23,7 +23,7 @@ def extractdata(context, data):
         result = {}
         street = _gettext((page.xpath('//tbody/tr['+str(j)+']/td[3]//p/text()')))
         result[street] = street
-        emit = data[result]
+        data = result
         context.emit(data=org_data)
 
 def _gettext(list):
