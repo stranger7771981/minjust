dc8ac6ebfd9a782f9129966653ada3dfa4ddf898
stranger7771981 <denis_uglov@mail>
2018-12-07 15:06:33 +0600

    Change Street_osh1

--- src/example/street_osh.py
+++ src/example/street_osh.py
@@ -8,7 +8,7 @@ def extractdata(context, data):
     # Parse the rest of the page to extract structured data.
 
    
-    street = _gettext(page.xpath("//div[@class='list-street']/p"))
+    street = _gettext(page.xpath("//tbody/tr['+str(j)+']/td[3]//p/text()"))
    
 
     org_data = {
